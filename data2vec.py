069173ce10ee06b9c195740cde21447358001ee5
dayoung <ekffkddl0529@naver.com>
2021-04-21 23:02:10 +0900

    modify data2ver.py

--- data2vec.py
+++ data2vec.py
@@ -15,7 +15,7 @@ model = BertModel.from_pretrained(
     config=pretrained_model_config,
 )
 
-sentences = ["무라세사에, 졸이쁨, 인기", "지켜본다, 으으으, 끔찍", "생각", "짱구", "?!", "양파", "제니", "양파죠", "불꽃놀이", "오마이걸", "엠넷", "엠카", "엠카운트다운", "갓세븐, 럴러바이, M COUNTDOWN"]
+sentences = ["무라세사에, 졸이쁨, 인기", "지켜본다, 으으으, 끔찍, 생, 짱구", "?!, 양파, 제니, 양파죠"]
 features = tokenizer(
     sentences,
     max_length=40,
