fba42f5d2b82fd69534c6a5b717bb54801f6e8fa
dayoung <ekffkddl0529@naver.com>
2021-05-01 21:27:59 +0900

    modify

--- data2vec.py
+++ data2vec.py
@@ -1,76 +1,80 @@
-# KcBERT embedding
-from transformers import BertTokenizer
-tokenizer = BertTokenizer.from_pretrained(
-    "beomi/kcbert-base",
-    do_lower_case=False,
-)
+def search_meme() :
+    # KcBERT embedding
+    from transformers import BertTokenizer
+    tokenizer = BertTokenizer.from_pretrained(
+        "beomi/kcbert-base",
+        do_lower_case=False,
+    )
 
-from transformers import BertConfig, BertModel
-pretrained_model_config = BertConfig.from_pretrained(
-    "beomi/kcbert-base"
-)
+    from transformers import BertConfig, BertModel
+    pretrained_model_config = BertConfig.from_pretrained(
+        "beomi/kcbert-base"
+    )
 
-import torch
-model = BertModel.from_pretrained(
-    "beomi/kcbert-base",
-    config=pretrained_model_config,
-)
+    import torch
+    model = BertModel.from_pretrained(
+        "beomi/kcbert-base",
+        config=pretrained_model_config,
+    )
 
-# data 불러오기
-import csv
-def csv2list() :
-    data = []
-    # encoding='utf-8-sig' 설정은 한글 깨짐 방지
-    f = open('tagData.csv', 'r')
-    rdr = csv.reader(f)
-    for line in rdr:
-        data.append(line)
-    f.close
+    # data 불러오기
+    import csv
+    def csv2list() :
+        data = []
+        # encoding='utf-8-sig' 설정은 한글 깨짐 방지
+        f = open('tagData.csv', 'r')
+        rdr = csv.reader(f)
+        for line in rdr:
+            data.append(line)
+        f.close
 
-    return data
+        return data
 
-sentences = csv2list()
-print(sentences)
-print("\n\n\n\n")
+    sentences = csv2list()
+    print(sentences)
+    print("\n\n\n\n")
 
-features = tokenizer(
-    sentences,
-    max_length=40,
-    padding="max_length",
-    truncation=True,
-)
+    features = tokenizer(
+        sentences,
+        max_length=40,
+        padding="max_length",
+        truncation=True,
+    )
 
-features = {k: torch.tensor(v) for k, v in features.items()}
+    features = {k: torch.tensor(v) for k, v in features.items()}
 
-outputs = model(**features)
+    outputs = model(**features)
 
-print(outputs[1])
+    print(outputs[1])
 
 
-# indexing
-import faiss
+    # indexing
+    import faiss
 
-tagVec = outputs[1].detach().numpy()
-index = faiss.IndexFlatL2(tagVec.shape[1])
-index.add(tagVec)
-print(index.ntotal)
+    tagVec = outputs[1].detach().numpy()
+    index = faiss.IndexFlatL2(tagVec.shape[1])
+    index.add(tagVec)
+    print(index.ntotal)
 
-# 임의로 설정한 검색어
-search = ['이쁘다', '짱구']
+    # 임의로 설정한 검색어
+    search = ['양파, 제니']
 
-#검색어 KcBERT로 imbedding
-search_features = tokenizer(
-    search,
-    max_length=40,
-    padding="max_length",
-    truncation=True,
-)
+    #검색어 KcBERT로 imbedding
+    search_features = tokenizer(
+        search,
+        max_length=40,
+        padding="max_length",
+        truncation=True,
+    )
 
-search_features = {k: torch.tensor(v) for k, v in search_features.items()}
-search_outputs = model(**search_features)
-print(search_outputs[1])
-searchVec = search_outputs[1].detach().numpy()
+    search_features = {k: torch.tensor(v) for k, v in search_features.items()}
+    search_outputs = model(**search_features)
+    print(search_outputs[1])
 
-# 검색어 비교 및 반환
-distances, indices = index.search(searchVec, 1)
-print(indices)
+    # 검색어 비교 및 반환
+    searchVec = search_outputs[1].detach().numpy()
+    distances, indices = index.search(searchVec, 1)
+    print(indices)
+
+
+search_meme()
