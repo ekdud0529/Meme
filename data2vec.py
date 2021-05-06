2f37a1d938fa7a7f3ef17d1fd1e79b5219f35f71
dayoung <ekffkddl0529@naver.com>
2021-05-06 16:00:05 +0900

    modify data2vec

--- data2vec.py
+++ data2vec.py
@@ -1,4 +1,4 @@
-def embedding(tagData) :
+def tagEmbedding(tagData) :
 
     # 토크나이저 초기화
     from transformers import BertTokenizer
@@ -32,43 +32,47 @@ def embedding(tagData) :
 
     return outputs
 
+def tagIndexing() : 
+
+    import faiss
+
+    tagVec = outputs[1].detach().numpy()
+    index = faiss.IndexFlatL2(tagVec.shape[1])
+    index.add(tagVec)
+    print(index.ntotal)
+    print("\n\n")
+
+    # faiss indexing save
+    faiss.write_index(index, "test.index")
+
+def readTag() :
+
+    import csv
+
+    data = []
+    # encoding='utf-8-sig' 설정은 한글 깨짐 방지
+    f = open('tagData.csv', 'r')
+    rdr = csv.reader(f)
+    for line in rdr:
+        data.append(line)
+    f.close
+
+    return data
+
 
 def search_meme() :
 
-    # data 불러오기
-    # import csv
-    # def csv2list() :
-    #     data = []
-    #     # encoding='utf-8-sig' 설정은 한글 깨짐 방지
-    #     f = open('tagData.csv', 'r')
-    #     rdr = csv.reader(f)
-    #     for line in rdr:
-    #         data.append(line)
-    #     f.close
-
-    #     return data
-
-    # outputs = embedding(csv2list())
-
-    # indexing
-    import faiss
-
-    # tagVec = outputs[1].detach().numpy()
-    # index = faiss.IndexFlatL2(tagVec.shape[1])
-    # index.add(tagVec)
-    # print(index.ntotal)
-    # print("\n\n")
-
-    # faiss indexing save
-    # faiss.write_index(index, "test.index")
+    #outputs = embedding(readTag())
 
     # faiss indexing read
+    import faiss
+
     index2 = faiss.read_index("test.index")
 
     # 임의로 설정한 검색어
     search = ['오마이걸 짤', '짱구 짤']
 
-    search_outputs = embedding(search)
+    search_outputs = tagEmbedding(search)
 
     # 검색어 비교 및 반환
     searchVec = search_outputs[1].detach().numpy()
