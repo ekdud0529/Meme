0f0bacdc0e4e1f20b0bb4dcdf5184ec6a60ccc3b
dayoung <ekffkddl0529@naver.com>
2021-05-06 15:54:18 +0900

    add index save and read

--- data2vec.py
+++ data2vec.py
@@ -36,28 +36,34 @@ def embedding(tagData) :
 def search_meme() :
 
     # data 불러오기
-    import csv
-    def csv2list() :
-        data = []
-        # encoding='utf-8-sig' 설정은 한글 깨짐 방지
-        f = open('tagData.csv', 'r')
-        rdr = csv.reader(f)
-        for line in rdr:
-            data.append(line)
-        f.close
+    # import csv
+    # def csv2list() :
+    #     data = []
+    #     # encoding='utf-8-sig' 설정은 한글 깨짐 방지
+    #     f = open('tagData.csv', 'r')
+    #     rdr = csv.reader(f)
+    #     for line in rdr:
+    #         data.append(line)
+    #     f.close
 
-        return data
+    #     return data
 
-    outputs = embedding(csv2list())
+    # outputs = embedding(csv2list())
 
     # indexing
     import faiss
 
-    tagVec = outputs[1].detach().numpy()
-    index = faiss.IndexFlatL2(tagVec.shape[1])
-    index.add(tagVec)
-    print(index.ntotal)
-    print("\n\n")
+    # tagVec = outputs[1].detach().numpy()
+    # index = faiss.IndexFlatL2(tagVec.shape[1])
+    # index.add(tagVec)
+    # print(index.ntotal)
+    # print("\n\n")
+
+    # faiss indexing save
+    # faiss.write_index(index, "test.index")
+
+    # faiss indexing read
+    index2 = faiss.read_index("test.index")
 
     # 임의로 설정한 검색어
     search = ['오마이걸 짤', '짱구 짤']
@@ -66,7 +72,7 @@ def search_meme() :
 
     # 검색어 비교 및 반환
     searchVec = search_outputs[1].detach().numpy()
-    distances, indices = index.search(searchVec, 3)
+    distances, indices = index2.search(searchVec, 3)
     print(indices)
     print("\n\n")
 
