ed8ce329ae7c40d1bf124a0e0ab4e86bfaa4e7ea
박예린 <carousel59@naver.com>
2021-05-16 08:27:20 +0000

    카카오톡과 연결

--- index.py
+++ index.py
@@ -4,9 +4,33 @@ ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 
 
 app = Flask(__name__)
 
+# 서버 작동 확인
 @app.route("/")
 def hello():
     return "Hello, Flask!"
+
+@app.route('/meme', methods=['POST'])
+def memeSearch() :
+    req = request.get_json()
+    
+    meme_value = req["action"]["detailParams"]["meme"]["value"]
+    
+    answer = meme_value + "wowowowowo"
+    
+    res = {
+        "version": "2.0"
+        "template": {
+            "outputs" : [
+                {
+                    "simpleText": {
+                        "text": answer
+                    }
+                }
+            ]
+        }
+    }
+    
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
