c9b3670dc0887a94a43de582b2c4ed98d2696724
ekdud0529 <45174177+ekdud0529@users.noreply.github.com>
2021-05-24 10:42:32 +0900

    Update memeCrawling.py

--- memeCrawling.py
+++ memeCrawling.py
@@ -13,17 +13,20 @@ def webCrawling() :
     # driver로 특정 페이지를 크롤링한다.
     driver.get('https://2runzzal.com/')
 
-    # Scroll down ##
-    SCROLL_PAUSE_SEC = 3
+    # Scroll down
+    SCROLL_PAUSE_SEC = 2
 
     # 스크롤 높이 가져옴
     last_height = driver.execute_script("return document.body.scrollHeight")
 
-    for i in range(7):
+    while True :
         # 끝까지 스크롤 다운
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        # 1초 대기
+        # 2초 대기
+        time.sleep(SCROLL_PAUSE_SEC)
+        # 한 번에 맨 마지막까지 스크롤되면 아래 리스트가 뜨지 않아, 마지막을 찍고 조금 창을 올리는 방법으로 리스트가 로딩될 수 있게 함
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);")
         time.sleep(SCROLL_PAUSE_SEC)
 
         # 스크롤 다운 후 스크롤 높이 다시 가져옴
@@ -32,7 +35,7 @@ def webCrawling() :
             break
         last_height = new_height
 
-    images = driver.find_elements_by_css_selector(".grid-item")
+    images = driver.find_elements_by_css_selector(".lazy")
     time.sleep(2)
 
     data = []
@@ -40,18 +43,19 @@ def webCrawling() :
 
     ## 이미지 가져오기
     cnt = 1
-    for i in range(imgLen):
-        img_data = []
+    for image in images:
+        imgData = []
 
         print(cnt)
         cnt += 1
 
         try:
-            # 이미지 src 가져오기
-            img_src = driver.find_elements_by_css_selector(".lazy")[i].get_attribute("src")
+            image.click()
             time.sleep(1)
 
-            driver.find_elements_by_css_selector(".lazy")[i].click()
+            # 이미지 src 가져오기
+            img_src = image.get_attribute("src")
+            time.sleep(1)
 
             # 이미지 tag 가져오기
             tagCnt = driver.find_elements_by_css_selector('#zcontents > section > div.grid > div.grid-item.on > div > div > div.zzal-info-bl > a')
@@ -73,22 +77,21 @@ def webCrawling() :
             img_tag.append(tag2str)
 
             # img와 tag를 한 list에 넣어주기
-            img_data.append(img_src)
-            img_data = img_data + img_tag
-
-            data.append(img_data)
+            imgData.append(img_src)
+            imgData = imgData + img_tag
+            print(imgData)
+            data.append(imgData)
 
         except:
             pass
     
-    print(data)
     driver.close()
     
     return data
 
 def list2csv(crawlingList) :
     # newline='' 설정이 없는 경우 row와 row 사이에 뉴라인이 한번 더 들어가게 됨
-    f = open('memeData.csv', 'w', newline='')
+    f = open('crawlingData.csv', 'w', newline='')
     wr = csv.writer(f)
     for imgList in crawlingList:
         wr.writerow(imgList)
@@ -106,4 +109,4 @@ def csv2list() :
     return data
 
 # webCrawling()
-# list2csv(webCrawling())
+list2csv(webCrawling())
